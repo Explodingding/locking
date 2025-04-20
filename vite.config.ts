6e6489dab88b62ed0e3a1a3ae39978d3381b8690
Explodingding <lukasz.klimowski@gmail.com>
2025-04-20 13:30:46 +0200

    Add Netlify configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ustawienie katalogu bazowego dla produkcji (na Netlify możemy ustawić '/')
+  base: '/',
 })
